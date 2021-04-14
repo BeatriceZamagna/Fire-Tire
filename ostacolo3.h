086afc43de279d7c998d0fa4b3520e501c12fe35
Beatrice Zamagna <80887796+BeatriceZamagna@users.noreply.github.com>
2021-04-14 23:14:56 +0200

    Delete ostacolo3.h

--- ostacolo3.h
+++
@@ -1,9 +0,0 @@
-#ifndef OSTACOLO3_H_INCLUDED
-#define OSTACOLO3_H_INCLUDED
-#include "ostacolo.h"
-
-class ostacolo3 : public ostacolo {
-	void ins_ostacolo(char x[20][80], int posx, int posy);
-};
-
-#endif // OSTACOLO3_H_INCLUDED
